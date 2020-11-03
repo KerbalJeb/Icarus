0f979a3729ff12e0c98243a179813799d02a2231
Ben Kellman <kellman.ben@gmail.com>
2020-11-03 13:33:07 -0800

    Added Asteroid Generation Class

---
+++ Assets/Scripts/AsteroidTester.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+[RequireComponent(typeof(Tilemap))]
+public class AsteroidTester : MonoBehaviour
+{
+    private                  Tilemap  tilemap;
+    [SerializeField] private TileBase tileBase;
+    public                   int      seed;
+    private                  int      lastSeed;
+    public                   int      xMax=30;
+    public                   int      yMax=30;
+    public                   int      noiseScale=15;
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void Start()
+    {
+        var asteroid = new AsteroidsGenerator(30, 30, 15);
+        asteroid.SetTilemap(ref tilemap, tileBase);
+    }
+
+    private void Update()
+    {
+        if (seed == lastSeed) return;
+        tilemap.ClearAllTiles();
+        var asteroid = new AsteroidsGenerator(xMax, yMax, noiseScale, seed);
+        asteroid.SetTilemap(ref tilemap, tileBase);
+        lastSeed = seed;
+    }
+}
+
---
+++ Assets/Scripts/AsteroidsGenerator.cs
@@ -0,0 +1,66 @@
+﻿﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class AsteroidsGenerator
+{
+    private Vector3Int[] points;
+    private float        LargeScaleNoise = 0.1f;
+    private float        SmallScaleNoise = 0.2f;
+    public AsteroidsGenerator(int xMax, int yMax, int noiseOffsetScale, int seed=0)
+    {
+        Random.InitState(seed);
+        noiseOffsetScale = Math.Min(noiseOffsetScale, Math.Min(xMax, yMax));
+        var squaredNoiseScale = noiseOffsetScale        * noiseOffsetScale;
+        var noiseOffset       = Random.insideUnitCircle * 1e3f;
+        
+        BoundsInt boundsInt = new BoundsInt(-xMax, -yMax, 0, 2*xMax, 2*yMax, 1);
+        var pointsList = new List<Vector3Int>();
+        
+        foreach (Vector3Int vector3Int in boundsInt.allPositionsWithin)
+        {
+            if (InsideAsteroid(vector3Int))
+            {
+                pointsList.Add(vector3Int);
+            }
+        }
+
+        points = pointsList.ToArray();
+        
+        bool InsideAsteroid(Vector3Int cords)
+        {
+            int   x     = cords.x;
+            int   y     = cords.y;
+            float angle = Mathf.Atan2(x, y);
+            float xPos  = (xMax - noiseOffsetScale) * Mathf.Cos(angle);
+            float yPos  = (yMax - noiseOffsetScale) * Mathf.Sin(angle);
+
+            float noise = Mathf.PerlinNoise(noiseOffset.x + xPos * LargeScaleNoise,
+                                            noiseOffset.y + yPos * LargeScaleNoise);
+            float smallNoise = Mathf.PerlinNoise(noiseOffset.x + xPos * SmallScaleNoise,
+                                                 noiseOffset.y + yPos * SmallScaleNoise);
+
+            float ellipseRad = xPos                * xPos
+                             + yPos                * yPos
+                             + (noise      - 0.5f) * squaredNoiseScale
+                             + (smallNoise - 0.5f) * (squaredNoiseScale / 4f);
+
+            float posRad = (x + 0.5f) * (x + 0.5f) + (y + 0.5f) * (y + 0.5f);
+            return posRad < ellipseRad;
+        }
+    }
+
+    public void SetTilemap(ref Tilemap tilemap, TileBase tileBase)
+    {
+        var tiles = new TileBase[points.Length];
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i] = tileBase;
+        }
+        tilemap.SetTiles(points, tiles);
+    }
+}
+
